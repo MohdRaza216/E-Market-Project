2d792659b2c855ed2b0481036d83b36d6f41e104
MohdRaza216 <mohdrazamoghul2024@gmail.com>
2024-10-27 11:41:04 +0530

    Home page setup initialize

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom";
+import { Text, VStack, Container } from "@chakra-ui/react";
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-l, #7928CA, #FF0080)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight="bold"
+          color="gray.500"
+        >
+          No Products Found 😥{" "}
+          <Link to={"/create"}>
+            <Text
+              as="span"
+              color="blue.500"
+              _hover={{ textDecoration: "underline" }}
+            >
+              Create a product
+            </Text>
+            
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
 };
 
 export default HomePage;
