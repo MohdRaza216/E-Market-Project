9058ab56de3ed0fd7603a0fa3921993b8d992db0
MohdRaza216 <mohdrazamoghul2024@gmail.com>
2024-11-21 15:36:57 +0530

    GitHub deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/E-Market-Project/',
   plugins: [react()],
   server: {
     proxy: {
